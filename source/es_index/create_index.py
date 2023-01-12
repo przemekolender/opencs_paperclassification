a9b5a58f7f89648106f723bb9f58f97e28885d11
przemekolender <56126908+przemekolender@users.noreply.github.com>
2023-01-13 00:43:34 +0100

    add function to bulk load data

--- source/es_index/create_index.py
+++ source/es_index/create_index.py
@@ -3,6 +3,7 @@ from source.es_index.IIndexBuilder import IIndexBuilder
 import requests 
 import json
 from source.env_setup.setup import connect_elasticsearch
+from elasticsearch.helpers import bulk
 
 
 def store_record(es_object, index, data):
@@ -18,6 +19,16 @@ def store_record(es_object, index, data):
     finally:
         return is_stored
 
+def store_records_bulk(es_object, index, data):
+    requests = []
+    for i, row in enumerate(data):
+        request = row
+        request["_op_type"] = "index"
+        request["_index"] = index
+        requests.append(request)
+    bulk(es_object, requests)
+
+
 def build_index(index_builder: IIndexBuilder, es_config: Dict[str, str]={'host': 'localhost', 'port': 9200}, idx_name: str='ontology'):
     try:
         template = index_builder.get_template()
@@ -25,8 +36,10 @@ def build_index(index_builder: IIndexBuilder, es_config: Dict[str, str]={'host':
         rows = index_builder.build_rows()
 
         es = connect_elasticsearch(es_config)
-        for row in rows:
-            store_record(es, idx_name, row)
+        store_records_bulk(es, idx_name, rows)
+
+        #for row in rows:
+        #    store_record(es, idx_name, row)
             
     except Exception as e:
         print(str(e))
